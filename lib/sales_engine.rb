49cf58ee3f67250ceb1c0b489fabb8b26481ff05
Marc Garreau <marcdgarreau@gmail.com>
2014-06-30 13:42:07 -0600

    Sales engine class cleanup

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,3 +1,10 @@
+require_relative 'merchant_repository'
+require_relative 'customer_repository'
+require_relative 'item_repository'
+require_relative 'invoice_repository'
+require_relative 'invoice_item_repository'
+require_relative 'transaction_repository'
+
 class SalesEngine
 
   def startup
@@ -9,12 +16,9 @@ class SalesEngine
     @merchant_repository = MerchantRepository.new
     #Returns array of merchants
   end
-
-
 end
 
-
-
+#For testing the SalesEngine class:
 if __FILE__ == $0
   engine = SalesEngine.new
   engine.startup
