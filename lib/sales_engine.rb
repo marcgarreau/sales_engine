888de7e2be49e6f1c7baceecccfa74fe67f75a53
Marc Garreau <marcdgarreau@gmail.com>
2014-06-30 10:57:44 -0600

    Introduces sales engine concepts

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,2 +1,22 @@
 class SalesEngine
+
+  def startup
+
+  end
+
+  def merchant_repository #after called, now you can use it
+    @merchant_repository = MerchantRepository.new
+    #Returns array of merchants
+  end
+
+
 end
+
+
+
+# if __FILE__ == $0
+  engine = SalesEngine.new
+  engine.startup
+
+  @merchant_repository.all
+# end
