fd5d13b6610c30eba532945f066004fd1b63bfc6
Corey Davis <coreyd303@gmail.com>
2014-06-30 14:27:20 -0600

    pseudo code for finder.rb

--- lib/finder.rb
+++ lib/finder.rb
@@ -1,5 +1,37 @@
 module Finder
 
+
+  # Customers
+  # random
+  # find by last name
+  # find all by first name
+
+  # Merchants
+  # random
+  # find by name
+  # find all by name
+
+  # Item
+  # random
+  # find by unit price
+  # find all by name
+
+  # Transactions
+  # random
+  # find by CC number
+  # find all by result
+
+  # Invoice
+  # random
+  # find by status
+  # find all by status
+
+  # Invoice item
+  # random
+  # find item id
+  # find all by quantity
+
+
   def all
     @merchants
   end
