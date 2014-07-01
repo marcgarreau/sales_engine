a2250602b64283eaa1cd166aa305a472c1346e38
Corey Davis <coreyd303@gmail.com>
2014-07-01 13:51:04 -0600

    adds customer_test

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -1,41 +1,47 @@
 require_relative 'test_helper.rb'
 
-class MerchantTest<Minitest::Test
+class CustomerTest<Minitest::Test
   attr_reader :data,
-              :merchant
+              :customer
 
   def setup
-    @merchant = Merchant.new(data)
+    @customer = Customer.new(data)
   end
 
   def data
-    { :id => "1",
-      :name => "Schroeder-Jerde",
+    { :id => "3",
+      :first_name => "Mariah",
+      :last_name => "Toy",
       :created_at => "2012-03-27 14:53:59 UTC",
       :updated_at => "2012-03-27 14:53:59 UTC"
     }
   end
 
   def test_it_has_attributes
-    merchant.id
-    merchant.name
-    merchant.created_at
-    merchant.updated_at
+    customer.id
+    customer.first_name
+    customer.last_name
+    customer.created_at
+    customer.updated_at
   end
 
   def test_it_knows_its_id
-    assert merchant.id == "1"
+    assert customer.id == "3"
   end
 
-  def test_it_knows_its_name
-    assert merchant.name == "Schroeder-Jerde"
+  def test_it_knows_its_first_name
+    assert customer.first_name == "Mariah"
+  end
+
+  def test_it_knows_its_last_name
+    assert customer.last_name == "Toy"
   end
 
   def test_it_knows_when_it_was_created
-    assert merchant.created_at == "2012-03-27 14:53:59 UTC"
+    assert customer.created_at == "2012-03-27 14:53:59 UTC"
   end
 
   def test_it_knows_when_it_was_updated
-    assert merchant.updated_at == "2012-03-27 14:53:59 UTC"
+    assert customer.updated_at == "2012-03-27 14:53:59 UTC"
   end
 end
