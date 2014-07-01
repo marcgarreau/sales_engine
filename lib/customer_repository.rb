2f2a47962ed6a990f9e3627af039acf35c2a2932
Marc Garreau <marcdgarreau@gmail.com>
2014-07-01 15:05:13 -0600

    Syntax & cleanup

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -19,13 +19,6 @@ class CustomerRepository
     @customers.shuffle.first
   end
 
-  # # RELATIONSHIPS
-  #   # item : find_by_id, invoices
-  #
-  # def find_by_id(customer_id)
-  #   @invoices = ????.find_all {|invoice| invoice.customer_id == customer_id }
-  # end
-
   def find_by_last_name(name)
     results = @customers.find {|customer| customer.last_name == name}
   end
@@ -36,9 +29,5 @@ class CustomerRepository
 
   def find_by_id(customer_id)
     results = @customers.find {|customer| customer.id == customer_id.to_s}
-  end
-
-  def invoices
-    
   end
 end
