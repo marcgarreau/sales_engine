03b7ec76df7889e43c9028e0831ff055f0692b0e
Corey Davis <coreyd303@gmail.com>
2014-07-07 16:26:03 -0600

    fixes output of finder

--- lib/finder.rb
+++ lib/finder.rb
@@ -15,11 +15,11 @@ module Finder
     def define_finders(*method_names)
       method_names.each do |method_name|
         define_method("find_by_#{method_name}") do |arg|
-          items.find { |item| item.public_send(method_name) == arg.to_s }
+          items.find { |item| item.public_send(method_name) == arg }
         end
 
         define_method("find_all_by_#{method_name}") do |arg|
-          items.find_all { |item| item.public_send(method_name) == arg.to_s }
+          items.find_all { |item| item.public_send(method_name) == arg }
         end
       end
     end
