c92c86ab8ab08fa3c15184c097138aef65a846a3
Corey Davis <coreyd303@gmail.com>
2014-07-09 11:53:48 -0600

    refactors merchant_count into unique method for favorite_merchant

--- lib/customer.rb
+++ lib/customer.rb
@@ -28,10 +28,13 @@ class Customer
     invoices.find_all(&:has_successful_charge?)
   end
 
-  def favorite_merchant
-    merchant_count = invoices_with_successful_charge.each_with_object(Hash.new(0)) do |invoice, merchant_counts_hash|
+  def merchant_count
+    invoices_with_successful_charge.each_with_object(Hash.new(0)) do |invoice, merchant_counts_hash|
       merchant_counts_hash[invoice.merchant_id] += 1
     end
+  end
+
+  def favorite_merchant
     top_id = merchant_count.max_by {|_,count| count}.first
     @repository.engine.merchant_repository.find_by_id(top_id)
   end
