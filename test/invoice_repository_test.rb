d1eea3c7d465367df7f6d20ecb1d4e548ab71298
Corey Davis <coreyd303@gmail.com>
2014-07-09 16:10:04 -0600

    adds testing for all find methods, all tests passing

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -10,9 +10,24 @@ class InvoiceRepositoryTest < Minitest::Test
     assert @repo.invoices.count >= 10
   end
 
+  def test_it_can_find_by_id
+    result = @repo.find_by_id(3)
+    assert_equal 78, result.merchant_id
+  end
+
+  def test_it_can_find_by_all_by_customer_id
+    results = @repo.find_all_by_customer_id(1)
+    assert_equal 8, results.count
+  end
+
+  def test_it_can_find_all_by_merchant_id
+    results = @repo.find_all_by_merchant_id(75)
+    assert_equal 48, results.count
+  end
+
   def test_it_can_find_by_status
     result = @repo.find_by_status("shipped")
-    assert_equal "76", result.merchant_id
+    assert_equal 26, result.merchant_id
   end
 
   def test_it_can_find_all_by_status
